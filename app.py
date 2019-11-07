28c514b87b586684ffd5753f70df55fbbfdcb6ad
MiriSilva <mirisb14@gmail.com>
2019-11-07 15:10:29 -0300

    Atualização das rotas

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html") 
+
+@app.route("/nova-mensagem")
+def novaMensagem():
+    return render_template("Mensagem.html")
+
+if __name__== "__main__":
+    app.run(debug=True)
